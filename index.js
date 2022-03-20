82033ed0430141ff9c1f3cbdb869ee3ef0bc30d8
Tuan Nguyen <tuan@users.noreply.github.com>
2022-03-19 21:40:01 -0700

    Cache DB query

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-import { Fzf, byLengthAsc } from 'fzf'
-import alfy from 'alfy';
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { Fzf, byLengthAsc } from "fzf";
+import alfy from "alfy";
 
 // const SAFARI_HISTORY_DB_PATH = '~/Library/Safari/History.db';
-const SAFARI_HISTORY_DB_PATH = '/tmp/safari-history.db'; // TODO: Need to bypass security during dev
+const SAFARI_HISTORY_DB_PATH = "/tmp/safari-history.db"; // TODO: Need to bypass security during dev
 const QUERY_LIMIT = 1000;
+const DB_QUERY_RESULT_CACHE_KEY = "db_query_result";
 
 const db = await open({
   filename: SAFARI_HISTORY_DB_PATH,
-  driver: sqlite3.Database
+  driver: sqlite3.Database,
 });
 
-const rows = await db.all(`
-  SELECT
-    visits.title,
-    items.url
-  FROM
-    history_items items
-  JOIN history_visits visits
-    ON visits.history_item = items.id
-  GROUP BY
-    visits.title
-  ORDER BY
-    visits.visit_time DESC
-  LIMIT ${QUERY_LIMIT}
-`);
+if (alfy.cache.get(DB_QUERY_RESULT_CACHE_KEY) == null) {
+  const rows = await db.all(`
+    SELECT
+      visits.title,
+      items.url
+    FROM
+      history_items items
+    JOIN history_visits visits
+      ON visits.history_item = items.id
+    GROUP BY
+      visits.title
+    ORDER BY
+      visits.visit_time DESC
+    LIMIT ${QUERY_LIMIT}
+  `);
 
-const fzf = new Fzf(rows, {
+  alfy.cache.set(DB_QUERY_RESULT_CACHE_KEY, rows, { maxAge: 30000 });
+}
+
+const fzf = new Fzf(alfy.cache.get(DB_QUERY_RESULT_CACHE_KEY), {
   selector: (item) => item.title,
-  tiebreakers: [byLengthAsc]
+  tiebreakers: [byLengthAsc],
 });
 
-const results = fzf.find(alfy.input ?? '');
+const results = fzf.find(alfy.input ?? "");
 
-const outputItems = results.map(({item})=> {
+const outputItems = results.map(({ item }) => {
   return {
-    'quicklookurl':item.url,
-    'uid':item.url,
-    'title':item.title,
-    'subtitle': item.url,
+    quicklookurl: item.url,
+    uid: item.url,
+    title: item.title,
+    subtitle: item.url,
   };
 });
 
-alfy.output(outputItems);
+alfy.output(outputItems.slice(0, 1));
