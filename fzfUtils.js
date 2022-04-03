7a067b6f892bddaf742756d50b2bdb0a9f5d5d65
Tuan Nguyen <tuan@users.noreply.github.com>
2022-04-02 19:16:36 -0700

    [refactoring] Move fzf and sql query to different module

---
+++ fzfUtils.js
@@ -0,0 +1,65 @@
+import alfy from "alfy";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { AsyncFzf, byStartAsc } from "fzf";
+
+// Script filter copies Safari History DB file to this location,
+// to get around permission issue
+const SAFARI_HISTORY_DB_PATH = "/tmp/safari-history.db";
+const DB_CACHE_KEY_PREFIX = "CACHED_FZF_INSTANCE";
+
+const db = await open({
+  filename: SAFARI_HISTORY_DB_PATH,
+  driver: sqlite3.Database,
+});
+
+/**
+ * Returns a cached Fzf instance for the given domain
+ *
+ * @export
+ * @param {*} domain
+ * @param {*} historyResultLimit max number of items in history to search
+ * @param {*} fzfResultLimit max number of fuzzy results to return
+ * @return {*} fzf instance
+ */
+export async function createFzfInstanceAsync(
+  domain,
+  historyResultLimit,
+  fzfResultLimit
+) {
+  const rows = await queryHistoryAsync(domain, historyResultLimit);
+  return new AsyncFzf(rows, {
+    selector: (item) => item.title,
+    tiebreakers: [byStartAsc],
+    limit: fzfResultLimit,
+  });
+}
+
+async function queryHistoryAsync(domain, historyResultLimit) {
+  const dbCacheKey = `${DB_CACHE_KEY_PREFIX}-${domain}-${historyResultLimit}`;
+  const cachedData = alfy.cache.get(dbCacheKey);
+  if (cachedData != null) {
+    return cachedData;
+  }
+
+  const sqlQuery = `
+    SELECT
+      visits.title,
+      items.url,
+      visits.visit_time
+    FROM
+      history_items items
+    JOIN history_visits visits
+      ON visits.history_item = items.id
+    WHERE
+      SUBSTR(SUBSTR(items.url, INSTR(items.url, '//') + 2), 0, INSTR(SUBSTR(items.url, INSTR(items.url, '//') + 2), '/')) LIKE '%${domain}%' AND
+      visits.title IS NOT NULL
+    GROUP BY
+      visits.title
+    ORDER BY
+      visits.visit_time DESC
+    LIMIT ${historyResultLimit}
+  `;
+
+  return await db.all(sqlQuery);
+}
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import { AsyncFzf, byStartAsc } from "fzf";
 import alfy from "alfy";
 import { formatRelative } from "date-fns";
+import { createFzfInstanceAsync } from "./fzfUtils.js";
 
-// Script filter copies Safari History DB file to this location,
-// to get around permission issue
-const SAFARI_HISTORY_DB_PATH = "/tmp/safari-history.db";
 const QUERY_LIMIT = 10000;
-const DB_QUERY_RESULT_CACHE_KEY = "db_query_result";
 const FZF_LIMIT = 15;
 
 /**
@@ -21,52 +15,17 @@ function convertAppleTimeToJsTime(appleTime) {
   return new Date(jsTimestamp);
 }
 
-const db = await open({
-  filename: SAFARI_HISTORY_DB_PATH,
-  driver: sqlite3.Database,
-});
+const input = (alfy.input ?? "").trim();
 
-const input = alfy.input ?? "";
 const keywords = input.split(" ").filter((kw) => kw.trim().length > 0);
-const hasDomain = keywords[0].startsWith("@");
+const hasDomain = keywords.length > 0 && keywords[0].startsWith("@");
 let domain = hasDomain ? keywords[0].substring(1) : "";
-
-const dbCacheKey = `${DB_QUERY_RESULT_CACHE_KEY}-${domain}`;
-if (alfy.cache.get(dbCacheKey) == null) {
-  const sqlQuery = `
-    SELECT
-      visits.title,
-      items.url,
-      visits.visit_time
-    FROM
-      history_items items
-    JOIN history_visits visits
-      ON visits.history_item = items.id
-    WHERE
-      SUBSTR(SUBSTR(items.url, INSTR(items.url, '//') + 2), 0, INSTR(SUBSTR(items.url, INSTR(items.url, '//') + 2), '/')) LIKE '%${domain}%' AND
-      visits.title IS NOT NULL
-    GROUP BY
-      visits.title
-    ORDER BY
-      visits.visit_time DESC
-    LIMIT ${QUERY_LIMIT}
-  `;
-  const rows = await db.all(sqlQuery);
-
-  alfy.cache.set(dbCacheKey, rows, { maxAge: 30000 });
-}
 
 // Scope search directive is `@domain<space>`, so +2
 const fzfQueryStart = hasDomain ? domain.length + 2 : 0;
 const fzfQuery = input.substring(fzfQueryStart);
-const cached = alfy.cache.get(dbCacheKey);
 
-const fzf = new AsyncFzf(cached, {
-  selector: (item) => item.title,
-  tiebreakers: [byStartAsc],
-  limit: FZF_LIMIT,
-});
-
+const fzf = await createFzfInstanceAsync(domain, QUERY_LIMIT, FZF_LIMIT);
 const results = await fzf.find(fzfQuery).catch(() => {});
 
 const iconPath = alfy.icon.get("GenericURLIcon");
